15d64fce4d2c35520fd2df2fcdc097390d7ef472
muce <imuce9@gmail.com>
2014-05-30 08:33:14 +0100

    use postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140307210535) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "instrument_patches", force: true do |t|
     t.integer  "instrument_id"
     t.integer  "patch_id"
